aa22845eb5441fde21ec5c9fa61da9253ccd3add
jhanvi283 <tjhanvi28@gmail.com>
2026-06-02 02:36:52 +0400

    Update JobMarketAnalysis code with switch and if-else logic

--- 24f25451_Jhanvi.java
+++ 24f25451_Jhanvi.java
@@ -1,25 +1,69 @@
-public class Jhanvi {
+import java.util.Scanner;
+
+public class JobMarketAnalysis {
 
     public static void main(String[] args) {
 
-        int sector = 1;
-        int vacancies;
+        Scanner sc = new Scanner(System.in);
 
-        switch(sector) {
+        // Take input from user
+        System.out.print("Enter sector (1=IT, 2=Finance, 3=Healthcare, 4=Education): ");
+        int sector = sc.nextInt();
+
+        System.out.print("Enter job postings this month: ");
+        int current = sc.nextInt();
+
+        System.out.print("Enter job postings last month: ");
+        int previous = sc.nextInt();
+
+        String sectorName;
+        double growthRate;
+
+        // Switch statement - find sector name
+        switch (sector) {
             case 1:
-                vacancies = 150;
+                sectorName = "IT";
                 break;
             case 2:
-                vacancies = 100;
+                sectorName = "Finance";
+                break;
+            case 3:
+                sectorName = "Healthcare";
+                break;
+            case 4:
+                sectorName = "Education";
                 break;
             default:
-                vacancies = 0;
+                sectorName = "Unknown";
+                break;
         }
 
-        if(vacancies > 120) {
-            System.out.println("High Demand Sector");
+        // Calculate growth rate
+        if (previous > 0) {
+            growthRate = ((double)(current - previous) / previous) * 100;
         } else {
-            System.out.println("Moderate Demand Sector");
+            growthRate = 0;
         }
+
+        // If-else - classify demand level
+        if (growthRate > 20) {
+            System.out.println("Sector: " + sectorName);
+            System.out.println("Growth: " + growthRate + "%");
+            System.out.println("Demand: HIGH DEMAND");
+        } else if (growthRate >= 0) {
+            System.out.println("Sector: " + sectorName);
+            System.out.println("Growth: " + growthRate + "%");
+            System.out.println("Demand: STABLE");
+        } else if (growthRate >= -20) {
+            System.out.println("Sector: " + sectorName);
+            System.out.println("Growth: " + growthRate + "%");
+            System.out.println("Demand: DECLINING");
+        } else {
+            System.out.println("Sector: " + sectorName);
+            System.out.println("Growth: " + growthRate + "%");
+            System.out.println("Demand: CRITICALLY LOW");
+        }
+
+        sc.close();
     }
 }
